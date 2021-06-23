40a59f3280b7bb5857656179bd47935b3730183a
Carlos Osuna <carlosalvaroosuna1@gmail.com>
2021-06-23 00:59:38 -0400

    cors

--- App/config/settings/base.py
+++ App/config/settings/base.py
@@ -56,7 +56,14 @@ MIDDLEWARE = [
 
 ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
-
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
 ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
