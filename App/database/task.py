035619575a635edd4be2696aedf2d5c8298337ba
Carlos Osuna <carlosalvaroosuna1@gmail.com>
2021-07-18 19:45:07 -0400

    Remove celery

--- App/database/task.py
+++ App/database/task.py
@@ -1,11 +1,9 @@
 from django.core.mail import send_mail
 from django.conf import settings
-from celery import shared_task
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 
-@shared_task
 def send_email_task():
 
     subject = 'Subject'
